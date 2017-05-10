1ed8121196f193e5d5566059963c9ce47abf706a
lcjqyml <0818lcjth>
2017-05-10 11:20:26 +0800

    tools: 添加maven清理工具类

---
+++ src/main/java/com/milin/tools/maven/CleanMvnFailedDependencies.java
@@ -0,0 +1,64 @@
+package com.milin.tools.maven;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+
+/**
+ * Created by Milin on 2017/5/10.
+ */
+public class CleanMvnFailedDependencies {
+
+    public static void main(String[] args) {
+        String m2Path = "";
+        if (StringUtils.isNotBlank(args[0])) {
+            m2Path = args[0];
+        }
+        findAndDelete(new File(m2Path));
+    }
+
+    private static boolean findAndDelete(File file) {
+        if (file.exists()) {
+            if (file.isFile()) {
+                if (file.getName().endsWith("lastUpdated")) {
+                    deleteFile(file.getParentFile());
+                    return true;
+                }
+            } else if (file.isDirectory()) {
+                File[] files = file.listFiles();
+                if (files != null) {
+                    for (File f : files) {
+                        if (findAndDelete(f)) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private static void deleteFile(File file) {
+        if (file.exists()) {
+            if (file.isFile()) {
+                print("删除文件:" + file.getAbsolutePath());
+                file.delete();
+            } else if (file.isDirectory()) {
+                File[] files = file.listFiles();
+                if (files != null) {
+                    for (File f : files) {
+                        deleteFile(f);
+                    }
+                }
+                print("删除文件夹:" + file.getAbsolutePath());
+                print("====================================");
+                file.delete();
+            }
+        }
+    }
+
+    private static void print(String msg) {
+        System.out.println(msg);
+    }
+
+}
